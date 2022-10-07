94d107df5fc40c83d1bcd0141d04c7449283bb95
ulysseyson <ulyseyson57@gmail.com>
2022-10-07 15:36:10 +0900

    style : mainpage UI feat : select category, click bookmark(not get request until now)
    -CategoriesButtion.tsx : get title, id from MainPage.tsx and give selected condition
    
    - NoticeItem.tsx : get title, date, isBookmarked form MainPage.tsx
    
    - MainPage.tsx : display mockupdata with BottomNavigationBar

---
+++ src/commons/NoticeItem.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ReactComponent as IsBookmarked } from '../assets/isBookmarked.svg';
+import { ReactComponent as IsNotBookmarked } from '../assets/isNotBookmarked.svg';
+
+const NoticeItem = ({
+  title,
+  date,
+  isBookmarked,
+  bookmark,
+}: {
+  title: string;
+  date: string;
+  isBookmarked: boolean;
+  bookmark: () => void;
+}) => {
+  return (
+    <div className="w-full px-[24px] h-[71px] flex flex-row" onClick={bookmark}>
+      <div>
+        <div className="text-black font-[700] text-[16px] leading-[23.17px]">{title}</div>
+        <div className="text-[#7E7E7E] font-[400] text-[10px] leading-[14.48px]">{date}</div>
+      </div>
+      <div className="pl-[26px] pt-[4px]" onClick={bookmark}>
+        {isBookmarked ? <IsBookmarked width="16" height="20" /> : <IsNotBookmarked width="16" height="20" />}
+      </div>
+    </div>
+  );
+};
+
+export default NoticeItem;
