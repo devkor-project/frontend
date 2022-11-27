1be6e71f5a22ea8c2fcf71a7501c975aa4269625
wjddnjs7895 <wjddnjs7895@naver.com>
2022-11-27 15:41:59 +0900

    feat PageStyle constant

---
+++ src/styles/index.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import { getHeightPixel, getWidthPixel } from '../utils/responsive';
+
+export const PageStyled = styled.div`
+  flex-direction: column;
+  width: 100%;
+  height: ${getHeightPixel(700)};
+  overflow-y: scroll;
+  align-items: center;
+  background-color: white;
+  border-radius: ${getWidthPixel(30)} ${getWidthPixel(30)} 0 0;
+  margin-top: ${getHeightPixel(80)};
+`;
