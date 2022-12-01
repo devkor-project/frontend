e746e6bc4747ddaf67d117d29092dd0daa4078f3
wjddnjs7895 <wjddnjs7895@naver.com>
2022-12-01 18:15:34 +0900

    add: constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,10 @@
 export const BASE__URL = 'https:/www.api.kudog.email/';
 
-export const GRADE__LIST = ['1학년', '2학년', '3학년', '4학년'];
+export const GRADE__LIST = [1, 2, 3, 4];
 
 export const MAJOR__LIST = ['컴퓨터학과', '디자인조형학부', '사이버국방학과'];
 
-export const STUDENT__ID__LIST = ['19학번', '20학번', '21학번', '22학번'];
+export const STUDENT__ID__LIST = [19, 20, 21, 22];
 
 export const MIN__STUDENT__ID = 19;
 
@@ -19,4 +19,22 @@ export const ROUTER__URI = {
   myPage: '/mypage',
   modifyUserInfoPage: '/mypage/modify',
   scrapPage: '/scrap',
+  findPassword: '/findpassword',
+};
+
+export const DEFAULT__USER__DATA = {
+  name: '',
+  email: '',
+  receiveEmail: '',
+  studentID: 0,
+  grade: 0,
+  major: '',
+  status: '',
+};
+
+export const DEFAULT__REGISTER__WARNING__CODE = {
+  emailWarningCode: '',
+  codeWarningCode: '',
+  formatWarningCode: '',
+  repeatWarningCode: '',
 };
