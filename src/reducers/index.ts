8887a8c1b26588e5df0b27c1dc5e3915c9b0af8b
wjddnjs7895 <wjddnjs7895@naver.com>
2022-12-01 18:42:16 +0900

    fix

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -14,5 +14,8 @@ const persistConfig = {
 };
 
 export default persistReducer(persistConfig, rootReducer);
+<<<<<<< HEAD
 
 export type RootState = ReturnType<typeof rootReducer>;
+=======
+>>>>>>> f1fd285 (feat: redux-persistent to store when f5)
