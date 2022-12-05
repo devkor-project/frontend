c737a54a5b2b1857c1a80a85f2599cfa91b9d26b
wjddnjs7895 <wjddnjs7895@naver.com>
2022-12-05 23:37:06 +0900

    fix: erase mapping

--- src/container/mypage/MyPageListContainer.tsx
+++ src/container/mypage/MyPageListContainer.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import ListButton from '../../components/Button/ListButton';
-import { MY__PAGE__BUTTON__LIST } from '../../constants';
+import { ROUTER__URI } from '../../constants';
+import { MY__PAGE__TEXT } from '../../constants/text';
 
 export default function MyPageListContainer() {
+  const [, , removeCookie] = useCookies(['refreshToken']);
+  const handleLogout = () => {
+    removeCookie('refreshToken', { path: '/' });
+    navigate(ROUTER__URI.loginPage);
+  };
+  const navigate = useNavigate();
   return (
     <ContainerStyled>
-      {MY__PAGE__BUTTON__LIST.map(text => {
-        return <ListButton key={text} text={text} />;
-      })}
+      <ListButton
+        text={MY__PAGE__TEXT.button.introduction[0]}
+        onClick={() => {
+          return 0;
+        }}
+      />
+      <ListButton
+        text={MY__PAGE__TEXT.button.faq[0]}
+        onClick={() => {
+          return 0;
+        }}
+      />
+      <ListButton
+        text={MY__PAGE__TEXT.button.contact[0]}
+        onClick={() => {
+          return 0;
+        }}
+      />
+      <ListButton text={MY__PAGE__TEXT.button.logout[0]} onClick={() => handleLogout()} />
     </ContainerStyled>
   );
 }
