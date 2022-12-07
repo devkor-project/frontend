6647fc1181cd3121ed4c86153ac2f48954a6b66b
wjddnjs7895 <wjddnjs7895@naver.com>
2022-12-07 18:41:26 +0900

    fix: category container

--- src/container/register/CategoryButtonContainer.tsx
+++ src/container/register/CategoryButtonContainer.tsx
@@ -61,7 +61,7 @@ const ContainerStyled = styled.div<{ height?: string }>`
 const RowStyled = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
   width: ${getWidthPixel(345)};
   margin-bottom: ${getHeightPixel(10)};
 `;
