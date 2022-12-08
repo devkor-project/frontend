838fa6e84d6c05b3997d5521fd01b6e50dad8da2
wjddnjs7895 <wjddnjs7895@naver.com>
2022-12-08 16:27:56 +0900

    feat: modify subscribe category api

--- src/utils/api_category.ts
+++ src/utils/api_category.ts
@@ -11,8 +11,26 @@ export async function getCategoryAPI({ setList }: { setList: Dispatch<SetStateAc
 }
 
 export async function getSubscribeCategoryAPI({ setList }: { setList: Dispatch<SetStateAction<CategoryDataProps[]>> }) {
-  const { data } = await axios.get(`ategory/subscribe`);
+  const { data } = await axios.get(`category/subscribe`);
   if (data) {
     setList(data.data);
+  }
+}
+
+export async function modifySubscribeCategoryAPI({
+  removeCatIds,
+  subscribeCatIds,
+  submitFunc,
+}: {
+  removeCatIds: number[];
+  subscribeCatIds: number[];
+  submitFunc: () => void;
+}) {
+  const { data } = await axios.put(`category/subscribe/many`, {
+    removeCatIds: removeCatIds,
+    newCatIds: subscribeCatIds,
+  });
+  if (data) {
+    submitFunc();
   }
 }
