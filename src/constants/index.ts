189b10063e0f8cf933a30e7d2397d67e8336f01c
wjddnjs7895 <wjddnjs7895@naver.com>
2022-12-13 12:21:28 +0900

    fix: erase major list

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,8 +4,6 @@ export const BASE__URL = 'https://www.api.kudog.email/';
 
 export const GRADE__LIST = [1, 2, 3, 4];
 
-export const MAJOR__LIST = ['컴퓨터학과', '디자인조형학부', '사이버국방학과'];
-
 export const STUDENT__ID__LIST = [17, 18, 19, 20, 21, 22];
 
 export const MIN__STUDENT__ID = 19;
