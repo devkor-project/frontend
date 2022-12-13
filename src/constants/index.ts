2db7e5a6961f82bce5d300e1a9036497a73ab01b
wjddnjs7895 <wjddnjs7895@naver.com>
2022-12-13 14:15:56 +0900

    fix: add external uri

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -22,6 +22,12 @@ export const ROUTER__URI = {
   categoryPage: '/subscribeCategory',
 };
 
+export const EXTERNAL__URI = {
+  introduction: 'https://placid-juice-fb4.notion.site/Kudog-c54d2cdfc8a149cf98a8d9d50d24265a',
+  faq: 'https://placid-juice-fb4.notion.site/FAQ-8b1c41b742da460388e6f9b0e9fee74d',
+  contact: 'https://forms.gle/ZP4PLKMnSWofEf6K6',
+};
+
 export const DEFAULT__USER__DATA = {
   name: '',
   email: '',
