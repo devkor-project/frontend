c1f835b4c653727770acc3d85380a9431c3bb811
wjddnjs7895 <wjddnjs7895@naver.com>
2022-12-13 14:16:10 +0900

    feat: add external func

--- src/container/mypage/MyPageListContainer.tsx
+++ src/container/mypage/MyPageListContainer.tsx
@@ -3,7 +3,7 @@ import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import ListButton from '../../components/Button/ListButton';
-import { ROUTER__URI } from '../../constants';
+import { EXTERNAL__URI, ROUTER__URI } from '../../constants';
 import { MY__PAGE__TEXT } from '../../constants/text';
 import { logoutUser } from '../../utils';
 
@@ -18,21 +18,12 @@ export default function MyPageListContainer() {
     <ContainerStyled>
       <ListButton
         text={MY__PAGE__TEXT.button.introduction[0]}
-        onClick={() => {
-          return 0;
-        }}
+        onClick={() => window.open(EXTERNAL__URI.introduction, '_blank')}
       />
-      <ListButton
-        text={MY__PAGE__TEXT.button.faq[0]}
-        onClick={() => {
-          return 0;
-        }}
-      />
+      <ListButton text={MY__PAGE__TEXT.button.faq[0]} onClick={() => window.open(EXTERNAL__URI.faq, '_blank')} />
       <ListButton
         text={MY__PAGE__TEXT.button.contact[0]}
-        onClick={() => {
-          return 0;
-        }}
+        onClick={() => window.open(EXTERNAL__URI.contact, '_blank')}
       />
       <ListButton text={MY__PAGE__TEXT.button.logout[0]} onClick={() => handleLogout()} />
     </ContainerStyled>
